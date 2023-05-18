5d94117ffeee829c5c27285bbd6f86eb7c76bf1b
Akash Desai <103101706+Akashh2151@users.noreply.github.com>
2023-05-18 02:05:19 -0700

    update forgetpassword_model.py

--- model/forgetpassword_model.py
+++ model/forgetpassword_model.py
@@ -1,20 +1,20 @@
-# import smtplib
-# from email.mime.text import MIMEText
+import smtplib
+from email.mime.text import MIMEText
 
-# def send_email(subject, recipient, message):
-#     try:
-#         sender_email = "akashdesai2151@gmail.com"
-#         sender_password = "okhnsnnviavjfsej"
+def send_email(subject, recipient, message):
+    try:
+        sender_email = "akashdesai2151@gmail.com"
+        sender_password = "okhnsnnviavjfsej"
 
-#         msg = MIMEText(message)
-#         msg['From'] = sender_email
-#         msg['To'] = recipient
-#         msg['Subject'] = subject
+        msg = MIMEText(message)
+        msg['From'] = sender_email
+        msg['To'] = recipient
+        msg['Subject'] = subject
 
-#         server = smtplib.SMTP('smtp.gmail.com', 587)
-#         server.starttls()
-#         server.login(sender_email, sender_password)
-#         server.sendmail(sender_email, recipient, msg.as_string())
-#         server.quit()
-#     except Exception as e:
-#         print("Email sending failed:", str(e))
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, recipient, msg.as_string())
+        server.quit()
+    except Exception as e:
+        print("Email sending failed:", str(e))
