eb9a4744e58c9e1d675240a51c602e214953262e
Akash Desai <103101706+Akashh2151@users.noreply.github.com>
2023-06-23 02:21:00 -0700

    update signinsginup_model.py

--- model/signInsignup_model.py
+++ model/signInsignup_model.py
@@ -42,49 +42,49 @@ collection = db["signup"]
 #             print(f"Error in find_user_by_email: {str(e)}")
 #             return None
 
-    def register_user(self, auth_data):
-        try:
-            # Check if the email already exists
-            existing_user = self.find_user_by_email(auth_data["email"])
-            if existing_user:
-                return {'error': 'Email already registered'}
+    # def register_user(self, auth_data):
+    #     try:
+    #         # Check if the email already exists
+    #         existing_user = self.find_user_by_email(auth_data["email"])
+    #         if existing_user:
+    #             return {'error': 'Email already registered'}
 
-            # Hash the password using SHA-256
-            password = auth_data["password"].encode('utf-8')  # Convert to bytes
-            hashed_password = hashlib.sha256(password).hexdigest()
+    #         # Hash the password using SHA-256
+    #         password = auth_data["password"].encode('utf-8')  # Convert to bytes
+    #         hashed_password = hashlib.sha256(password).hexdigest()
 
-            # Add registrationDate field with the current date and time
-            auth_data["registrationDate"] = datetime.utcnow()
+    #         # Add registrationDate field with the current date and time
+    #         auth_data["registrationDate"] = datetime.utcnow()
 
-            # Create user document with additional fields
-            user = {
-                "firstName": auth_data["firstName"],
-                "lastName": auth_data["lastName"],
-                "email": auth_data["email"],
-                "password": hashed_password,  # Store the hashed password
-                "userName": auth_data["userName"],
-                "role": auth_data["role"],
-                "company": auth_data["company"],
-                "businessCategory": auth_data["businessCategory"],
-                "bundle": auth_data["bundle"],
-                "emailverified": auth_data.get("emailverified", False),
-                "locale": auth_data.get("locale", "en-US"),
-                "picture": auth_data.get("picture", ""),
-                "expirydate": auth_data.get("expirydate"),
-                "registrationDate": auth_data["registrationDate"],  # Add registrationDate here
-            }
+    #         # Create user document with additional fields
+    #         user = {
+    #             "firstName": auth_data["firstName"],
+    #             "lastName": auth_data["lastName"],
+    #             "email": auth_data["email"],
+    #             "password": hashed_password,  # Store the hashed password
+    #             "userName": auth_data["userName"],
+    #             "role": auth_data["role"],
+    #             "company": auth_data["company"],
+    #             "businessCategory": auth_data["businessCategory"],
+    #             "bundle": auth_data["bundle"],
+    #             "emailverified": auth_data.get("emailverified", False),
+    #             "locale": auth_data.get("locale", "en-US"),
+    #             "picture": auth_data.get("picture", ""),
+    #             "expirydate": auth_data.get("expirydate"),
+    #             "registrationDate": auth_data["registrationDate"],  # Add registrationDate here
+    #         }
 
-            # Insert user document into the database
-            result = collection.insert_one(user)
+    #         # Insert user document into the database
+    #         result = collection.insert_one(user)
 
-            # Convert the ObjectId to a string
-            user_id = str(result.inserted_id)
+    #         # Convert the ObjectId to a string
+    #         user_id = str(result.inserted_id)
 
-            # Return the user data including the generated ObjectId as a string
-            user['_id'] = user_id
+    #         # Return the user data including the generated ObjectId as a string
+    #         user['_id'] = user_id
 
-            return user
-        except Exception as e:
-            # Handle the exception, log the error, and return an error response
-            print(f"Error in register_user: {str(e)}")
-            return {'error': str(e)}
+    #         return user
+    #     except Exception as e:
+    #         # Handle the exception, log the error, and return an error response
+    #         print(f"Error in register_user: {str(e)}")
+    #         return {'error': str(e)}
