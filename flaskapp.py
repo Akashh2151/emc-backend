58ce210be7c3b963cd0d0bdbda7d20a3cc69e342
Akash Desai <103101706+Akashh2151@users.noreply.github.com>
2023-07-14 02:24:11 -0700

    update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -6,7 +6,7 @@ from controller.shop import shopapp
 from controller.resto import restoapp
 
 app = Flask(__name__)
-# app.secret_key = '98c5bc0a178ff2d6c0c1471c6f3dc5e4'
+app.secret_key = '98c5bc0a178ff2d6c0c1471c6f3dc5e4'
 
 app.register_blueprint(signUp_bp)
 app.register_blueprint(login_bp)
