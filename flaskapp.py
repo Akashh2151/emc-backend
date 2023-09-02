91bba141facab6e8b74549027250980ceb8496fb
Akash Desai <103101706+Akashh2151@users.noreply.github.com>
2023-09-01 22:01:55 -0700

    adding some changes

---
+++ flaskapp.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return 'Hello World Again'
+
+# main driver function
+if __name__ == '__main__':
+	app.run()
