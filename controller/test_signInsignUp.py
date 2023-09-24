56a5b14675f9f147a8ec3907919a26a0c79d39fd
Akash Desai <103101706+Akashh2151@users.noreply.github.com>
2023-09-24 03:14:45 -0700

    Create test_signInsignUp.py

---
+++ controller/test_signInsignUp.py
@@ -0,0 +1,36 @@
+import pytest
+from model.signInsignup_model import AuthModel, SignupModel
+
+# Initialize test instances of AuthModel and SignupModel with a test database
+@pytest.fixture
+def auth_model_instance():
+    return AuthModel(database_url="your_test_database_url")
+
+@pytest.fixture
+def signup_model_instance():
+    return SignupModel(database_url="your_test_database_url")
+
+# Test the AuthModel class
+def test_auth_model(auth_model_instance):
+    # Write test cases for AuthModel methods
+    pass
+
+# Test the SignupModel class
+def test_signup_model(signup_model_instance):
+    # Write test cases for SignupModel methods
+    pass
+
+# You can also write integration tests for your Flask routes using a test client
+@pytest.fixture
+def test_client():
+    from your_flask_app import app  # Replace with your Flask app import
+    app.config['TESTING'] = True
+    return app.test_client()
+
+def test_register_route(test_client):
+    # Write test cases for the /register route
+    pass
+
+def test_login_route(test_client):
+    # Write test cases for the /login route
+    pass
