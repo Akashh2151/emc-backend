f14fc8edde8f0cb1959e7c06f8c9fa232ff4eaf0
Akash Desai <103101706+Akashh2151@users.noreply.github.com>
2023-10-17 16:26:31 -0700

    used try except

--- controller/signInsignUp.py
+++ controller/signInsignUp.py
@@ -1,56 +1,59 @@
-import hashlib  # Import the hashlib library
+import hashlib
 from model.signInsignup_model import AuthModel, SignupModel
-from flask import jsonify, request
+from flask import Blueprint, jsonify, request
 
 auth_model = AuthModel()
 signup_model = SignupModel()
 
-from flask import Blueprint
-
 signUp_bp = Blueprint('signUp', __name__)
-
 
 @signUp_bp.route("/register", methods=["POST"])
 def register():
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    # Check if the email already exists in the database
-    existing_user = signup_model.find_user_by_email(data.get("email"))
-    if existing_user:
-        return jsonify({'error': 'Email already registered'}), 400
+        # Check if the email already exists in the database
+        existing_user = signup_model.find_user_by_email(data.get("email"))
+        if existing_user:
+            return jsonify({'error': 'Email already registered'}), 400
 
-    result = signup_model.register_user(data)
+        result = signup_model.register_user(data)
 
-    if "error" in result:
-        return jsonify({'error': result['error']}), 400
+        if "error" in result:
+            return jsonify({'error': result['error']}), 400
 
-    return jsonify({'message': 'Registration successful'})
+        return jsonify({'message': 'Registration successful'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 login_bp = Blueprint('login', __name__)
 
 @login_bp.route("/login", methods=["POST"])
 def login():
-    data = request.get_json()
-    email = data.get("email")
-    password = data.get("password")
+    try:
+        data = request.get_json()
+        email = data.get("email")
+        password = data.get("password")
 
-    user = auth_model.login_user(email, password)
+        user = auth_model.login_user(email, password)
 
-    if user:
-        # Hash the provided password for comparison
-        provided_password = password.encode('utf-8')
-        hashed_provided_password = hashlib.sha256(provided_password).hexdigest()
+        if user:
+            # Hash the provided password for comparison
+            provided_password = password.encode('utf-8')
+            hashed_provided_password = hashlib.sha256(provided_password).hexdigest()
 
-        # Compare the hashed provided password with the stored hashed password
-        if hashed_provided_password == user["password"]:
-            role = user["role"]  # Update to use the "role" directly from the user
-            response = {'message': 'Login successful'}
-            if "owner" in role:
-                response["role_info"] = "Welcome Owner"
-            elif "managerOne" in role:
-                response["role_info"] = "Welcome Manager"
-            elif "staffOne" in role:
-                response["role_info"] = "Welcome Staff"
-            return jsonify(response)
-    
-    return jsonify({'error': 'Invalid email or password'}), 404
+            # Compare the hashed provided password with the stored hashed password
+            if hashed_provided_password == user["password"]:
+                role = user["role"]  # Update to use the "role" directly from the user
+                response = {'message': 'Login successful'}
+                if "owner" in role:
+                    response["role_info"] = "Welcome Owner"
+                elif "managerOne" in role:
+                    response["role_info"] = "Welcome Manager"
+                elif "staffOne" in role:
+                    response["role_info"] = "Welcome Staff"
+                return jsonify(response)
+
+        return jsonify({'error': 'Invalid email or password'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
--- model/signInsignup_model.py
+++ model/signInsignup_model.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
-# from flask_bcrypt import Bcrypt
 from datetime import datetime
 import hashlib
+
 # Initialize MongoDB connection
 client = MongoClient("mongodb+srv://akashh2151:aOSefZ94SgQEkzmg@cluster0.25xmos0.mongodb.net/?retryWrites=true&w=majority")
 
@@ -11,70 +11,80 @@ db = client["emc_project"]
 # Select the "signup" collection within the "emc_project" database
 collection = db["signup"]
 
-# Initialize Flask-Bcrypt
-# bcrypt = Bcrypt()
-
 class AuthModel:
     def login_user(self, email, password):
-        # Find the user by email
-        user = collection.find_one({"email": email})
+        try:
+            # Find the user by email
+            user = collection.find_one({"email": email})
 
-        # Check if the user exists
-        if user:
-            # Hash the provided password using the same method as during registration
-            provided_password = password.encode('utf-8')  # Convert to bytes
-            hashed_provided_password = hashlib.sha256(provided_password).hexdigest()
+            # Check if the user exists
+            if user:
+                # Hash the provided password using the same method as during registration
+                provided_password = password.encode('utf-8')  # Convert to bytes
+                hashed_provided_password = hashlib.sha256(provided_password).hexdigest()
 
-            # Compare the hashed provided password with the stored hashed password
-            if hashed_provided_password == user["password"]:
-                return user
+                # Compare the hashed provided password with the stored hashed password
+                if hashed_provided_password == user["password"]:
+                    return user
 
-        return None
-    
-    
+            return None
+        except Exception as e:
+            # Handle the exception, log the error, and return an error response
+            print(f"Error in login_user: {str(e)}")
+            return None
 
 class SignupModel:
     def find_user_by_email(self, email):
-        return collection.find_one({"email": email})
-    
+        try:
+            return collection.find_one({"email": email})
+        except Exception as e:
+            # Handle the exception, log the error, and return an error response
+            print(f"Error in find_user_by_email: {str(e)}")
+            return None
+
     def register_user(self, auth_data):
-        # Check if the email already exists
-        existing_user = self.find_user_by_email(auth_data["email"])
-        if existing_user:
-            return {'error': 'Email already registered'}
+        try:
+            # Check if the email already exists
+            existing_user = self.find_user_by_email(auth_data["email"])
+            if existing_user:
+                return {'error': 'Email already registered'}
 
-        # Hash the password using SHA-256
-        password = auth_data["password"].encode('utf-8')  # Convert to bytes
-        hashed_password = hashlib.sha256(password).hexdigest()
+            # Hash the password using SHA-256
+            password = auth_data["password"].encode('utf-8')  # Convert to bytes
+            hashed_password = hashlib.sha256(password).hexdigest()
 
-        # Add registrationDate field with the current date and time
-        auth_data["registrationDate"] = datetime.utcnow()
+            # Add registrationDate field with the current date and time
+            auth_data["registrationDate"] = datetime.utcnow()
 
-        # Create user document with additional fields
-        user = {
-            "firstName": auth_data["firstName"],
-            "lastName": auth_data["lastName"],
-            "email": auth_data["email"],
-            "password": hashed_password,  # Store the hashed password
-            "userName": auth_data["userName"],
-            "role": auth_data["role"],
-            "company": auth_data["company"],
-            "businessCategory": auth_data["businessCategory"],
-            "bundle": auth_data["bundle"],
-            "emailverified": auth_data.get("emailverified", False),
-            "locale": auth_data.get("locale", "en-US"),
-            "picture": auth_data.get("picture", ""),
-            "expirydate": auth_data.get("expirydate"),
-            "registrationDate": auth_data["registrationDate"],  # Add registrationDate here
-        }
+            # Create user document with additional fields
+            user = {
+                "firstName": auth_data["firstName"],
+                "lastName": auth_data["lastName"],
+                "email": auth_data["email"],
+                "password": hashed_password,  # Store the hashed password
+                "userName": auth_data["userName"],
+                "role": auth_data["role"],
+                "company": auth_data["company"],
+                "businessCategory": auth_data["businessCategory"],
+                "bundle": auth_data["bundle"],
+                "emailverified": auth_data.get("emailverified", False),
+                "locale": auth_data.get("locale", "en-US"),
+                "picture": auth_data.get("picture", ""),
+                "expirydate": auth_data.get("expirydate"),
+                "registrationDate": auth_data["registrationDate"],  # Add registrationDate here
+            }
 
-        # Insert user document into the database
-        result = collection.insert_one(user)
+            # Insert user document into the database
+            result = collection.insert_one(user)
 
-        # Convert the ObjectId to a string
-        user_id = str(result.inserted_id)
+            # Convert the ObjectId to a string
+            user_id = str(result.inserted_id)
 
-        # Return the user data including the generated ObjectId as a string
-        user['_id'] = user_id
+            # Return the user data including the generated ObjectId as a string
+            user['_id'] = user_id
 
-        return user
+            return user
+        except Exception as e:
+            # Handle the exception, log the error, and return an error response
+            print(f"Error in register_user: {str(e)}")
+            return {'error': str(e)}
