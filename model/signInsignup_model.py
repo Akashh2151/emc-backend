a67b6fc2957377c301370ff051f8adf854f5c394
Akash Desai <103101706+Akashh2151@users.noreply.github.com>
2023-11-08 19:20:23 -0800

    update signinsignup_model.py

--- model/signInsignup_model.py
+++ model/signInsignup_model.py
@@ -1,16 +1,26 @@
 from mongoengine import Document, StringField, EmailField, IntField, BooleanField
 from flask import Flask
 from flask_mongoengine import MongoEngine
-from werkzeug.security import generate_password_hash, check_password_hash
+# from werkzeug.security import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'emc_project2151',  # Use the specified MongoDB database
-    'host': 'localhost',
-    'port': 27017
-}
-db = MongoEngine()
-db.init_app(app)
+
+from mongoengine import connect
+# Replace the values with your username, password, and cluster name
+db_uri = "mongodb+srv://akashh2151:aOSefZ94SgQEkzmg@cluster0.25xmos0.mongodb.net/?retryWrites=true&w=majority"
+# Replace the value with your database name
+connect(host=db_uri, db="emc_project2151")
+
+
+# app.config['MONGODB_SETTINGS'] = {
+#     'db': 'emc_project2151',  # Use the specified MongoDB database
+#     'host': 'localhost',
+#     'port': 27017
+# }
+# db = MongoEngine()
+# db.init_app(app)
+
 
 # class User(Document):
     # name = StringField(required=True, max_length=100)
@@ -32,8 +42,8 @@ class User(Document):
     # age = IntField()
     # is_active = BooleanField(default=True)
 
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
+    # def set_password(self, password):
+    #     self.password = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.password, password)    
+    # def check_password(self, password):
+    #     return check_password_hash(self.password, password)    
