026a5ad83ba0cd1834f4c57e6032a286c8e95806
Akash Desai <103101706+Akashh2151@users.noreply.github.com>
2023-11-22 19:02:16 -0800

    update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,14 +1,16 @@
-from flask import Flask, Response, make_response
+from flask import Flask
 from flask_jwt_extended import JWTManager
 from controller.signInsignUp import signUp_bp  
 from controller.signInsignUp import login_bp
 from flask_cors import CORS
 from controller.shop import shopapp
  
- 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '98c5bc0a178ff2d6c0c1471c6f3dc5e4'
 
+
+
+app = Flask(__name__)
+# app.secret_key = '98c5bc0a178ff2d6c0c1471c6f3dc5e4'
+app.config['SECRET_KEY'] = '98c5bc0a178ff2d6c0c1471c6f3dc5e4'
 
 app.register_blueprint(signUp_bp)
 app.register_blueprint(login_bp)
@@ -32,21 +34,9 @@ app.config['JWT_ACCESS_TOKEN_EXPIRES'] = False  # Set your token expiration as n
 jwt = JWTManager(app)
 
 # Enable CORS for all routes
-# CORS(app)
-# CORS(app, origins=['http://localhost:3000', 'https://my-digital-ocean-app.com'])
-# Enable CORS for all routes
-CORS(app, origins=['http://localhost:3000', 'https://my-digital-ocean-app.com'], supports_credentials=True)
+CORS(app)
+CORS(app, origins=['http://localhost:3000', 'https://my-digital-ocean-app.com'])
 
-
-
-@app.route('/')
-def hello_world():
-    resp = make_response('Hello World Again')
-    
-    # Set the cookie with SameSite=None and Secure flag
-    resp.set_cookie('my_cookie', 'cookie_value', secure=True, samesite='None')
-    
-    return resp
 
 
 @app.route('/')
